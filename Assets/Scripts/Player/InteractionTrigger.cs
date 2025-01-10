d6e348d6584d48e9b0cea13a2d2fd7dc554031b6
KoalMCasler <Theeoblivion@gmail.com>
2025-01-10 12:06:23 -0400

    Modified player interaction

---
+++ Assets/Scripts/Player/InteractionTrigger.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionTrigger : MonoBehaviour
+{
+    public PlayerController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    /// <summary>
+    /// Event called when object is inside trigger for more than 1 frame. 
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.TryGetComponent<Intractable>(out Intractable pushable))
+        {
+            player.interactionPosable = true;
+            player.interactionTarget = pushable.gameObject;
+        }
+    }
+
+    /// <summary>
+    /// Event called when object exits trigger. 
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.TryGetComponent<Intractable>(out Intractable pushable))
+        {
+            player.interactionPosable = false;
+            player.interactionTarget = null;
+        }
+    }
+}
