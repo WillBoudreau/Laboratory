1b753361617be8588b33848ff85cdb977a70b872
KoalMCasler <Theeoblivion@gmail.com>
2025-02-04 16:53:05 -0400

    Update PlayerController.cs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -344,6 +344,7 @@ public class PlayerController : MonoBehaviour
             if(isGrounded)
             {
                 playerBody.AddForce(transform.up * jumpForce);
+                sFXManager.source2D.Stop();
             }
             if(isGrabbingLedge)
             {
