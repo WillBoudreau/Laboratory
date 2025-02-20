d9a5cca4346f58f6ca70712be5b51defedb0d1cf
KoalMCasler <Theeoblivion@gmail.com>
2025-02-20 11:55:11 -0400

    Update LevelManager.cs

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -49,6 +49,11 @@ public class LevelManager : MonoBehaviour
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
+        spawn = GameObject.FindWithTag("Spawn");
+        if(spawn != null)
+        {
+            player.transform.position = spawn.transform.position;
+        }
         if(scene.name.StartsWith("L_"))
         {
             player.SetActive(true);
@@ -63,11 +68,6 @@ public class LevelManager : MonoBehaviour
             player.SetActive(false);
         }
         Debug.Log("SceneLoaded");
-        spawn = GameObject.FindWithTag("Spawn");
-        if(spawn != null)
-        {
-            player.transform.position = spawn.transform.position;
-        }
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
