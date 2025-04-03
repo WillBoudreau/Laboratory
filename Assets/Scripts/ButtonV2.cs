40f83c26d23649f71e998d5d8a04b8d612636b61
max5max46 <113365542+max5max46@users.noreply.github.com>
2025-04-03 08:23:16 -0300

    Updated Infographics

---
+++ Assets/Scripts/ButtonV2.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class ButtonV2 : MonoBehaviour
+{
+    [Header("Button Settings")]
+    [SerializeField] private GameObject[] objectsToControl; // The object the button controls
+    [SerializeField] private float upwardVelocity = 1;
+
+    [Header("Audio Settings")]
+    [SerializeField] private SFXManager sfxManager;//The SFX manager
+
+    private Rigidbody rb;
+    [HideInInspector] public bool isActivated;
+
+    void Awake()
+    {
+        sfxManager = GameObject.Find("SFXManager").GetComponent<SFXManager>();
+
+        isActivated = false;
+        rb = transform.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localPosition.y > 0.165f)
+        {
+            transform.localPosition = new Vector3(0, 0.165f, 0);
+        }
+
+        if (transform.localPosition.y < -0.03f)
+        {
+            transform.localPosition = new Vector3(0, -0.03f, 0);
+        }
+
+        if (transform.localPosition.y <= -0.01f && !isActivated)
+        {
+            ButtonActivated();
+        }
+
+        if (transform.localPosition.y > 0.05f && isActivated)
+        {
+            ButtonDeactivated();
+        }
+
+        rb.velocity = new Vector3(0, upwardVelocity, 0);
+
+    }
+
+    private void ButtonActivated()
+    {
+        sfxManager.Player2DSFX(sfxManager.buttonSFX[1], false);
+
+        //For each object in the array
+        foreach (GameObject obj in objectsToControl)
+        {
+            switch (obj.tag)
+            {
+                //If the object is a door, open it
+                case "Door":
+                    obj.GetComponent<DoorBehaviour>().OpenThisDoor();
+                    break;
+
+                //If the object is a platform, allow it to move
+                case "Platform":
+                    obj.GetComponent<MovingPlatform>().SetMovementStatus(true);
+                    break;
+
+                case "Receiver":
+                    obj.GetComponent<LaserEmitter>().FireLaser();
+                    break;
+
+                case "BoxDispenser":
+                    obj.GetComponent<BoxSpawner>().SpawnBox();
+                    break;
+
+                default:
+                    Debug.LogError("Object under objectsToControl not tagged correctly or missing behaviour");
+                    break;
+            }
+        }
+
+        isActivated = true;
+    }
+
+    private void ButtonDeactivated()
+    {
+        sfxManager.Player2DSFX(sfxManager.buttonSFX[1], false);
+
+        //For each object in the array
+        foreach (GameObject obj in objectsToControl)
+        {
+            switch (obj.tag)
+            {
+                //If the object is a door, close it
+                case "Door":
+                    obj.GetComponent<DoorBehaviour>().CloseThisDoor();
+                    break;
+
+                //If the object is a platform, stop it from moving
+                case "Platform":
+                    obj.GetComponent<MovingPlatform>().SetMovementStatus(false);
+                    break;
+
+                case "Receiver":
+                    obj.GetComponent<LaserEmitter>().DisableLaser();
+                    break;
+
+                case "BoxDispenser":
+                    break;
+
+                default:
+                    Debug.LogError("Object under objectsToControl not tagged correctly or missing behaviour");
+                    break;
+            }
+        }
+
+        isActivated = false;
+    }
+}
