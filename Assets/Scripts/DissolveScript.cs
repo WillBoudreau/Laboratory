975671c247cdded74acb9023a66e41cb89ccc230
Will Conrad <144450261+Wynamo@users.noreply.github.com>
2025-04-09 10:05:40 -0300

    Disintegration shader done

--- Assets/Scripts/DissolveScript.cs
+++ Assets/Scripts/DissolveScript.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class DissolveScript : MonoBehaviour
 {
     public SkinnedMeshRenderer skinnedMesh;
+    public VisualEffect VFXGraph;
     public float dissolveRate = 0.0125f;
     public float refreshRate = 0.025f;
 
@@ -25,15 +27,20 @@ public class DissolveScript : MonoBehaviour
 
     IEnumerator DissolveCo()
     {
+        if (VFXGraph != null)
+        {
+            VFXGraph.Play();
+        }
         if (skinnedMaterials.Length > 0)
         {
             float counter = 0;
-            while (skinnedMaterials[0].GetFloat("_DissolveAmount") < 1)
+
+            while (skinnedMaterials[0].GetFloat("_Dissolve_Amount") < 1)
             {
                 counter += dissolveRate;
                 for (int i = 0; i < skinnedMaterials.Length; i++)  // Fixed loop condition here
                 {
-                    skinnedMaterials[i].SetFloat("_DissolveAmount", counter);
+                    skinnedMaterials[i].SetFloat("_Dissolve_Amount", counter);
                 }
                 yield return new WaitForSeconds(refreshRate);
             }
